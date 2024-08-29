e06c3e490fec5458875875f6b64139c41862e014
hetsuthar1533 <108316485+hetsuthar1533@users.noreply.github.com>
2024-08-29 15:36:46 +0530

    Backend 1

--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
-import list   from '../../public/list.json'
+import React, { useEffect, useState } from 'react'
+// import list   from '../../public/list.json'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import Cards from './cards';
+import axios from 'axios'
 function Freebook() {
-    const filterData = list.filter((data)=>data.category === "Free")
+  const [book,setBook] = useState([])
+  useEffect(()=>{
+          const getBook = async()=>{
+            try{
+            const res =  await axios.get("http://localhost:4001/book")  
+            const data = res.data.filter((data)=>data.category === "Free")
+            console.log(data)
+            setBook(data)
+            }catch(error){
+                  console.log(error)
+            }
+          }
+          getBook();
+  },[])
+
  
     var settings = {
         dots: true,
@@ -51,7 +66,7 @@ function Freebook() {
     
     <div>
     <Slider {...settings}>
-        {filterData.map((item)=>{
+        {book.map((item)=>{
           return  <Cards  item={item} key={item.id}/>
         })}
       </Slider>
--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useForm } from "react-hook-form";
 import Login from './Login';
+import axios from 'axios';
 
 function Signup() {
     const {
@@ -10,7 +11,25 @@ function Signup() {
         formState: { errors },
     } = useForm();
 
-    const onSubmit = (data) => console.log(data);
+    const onSubmit = async (data) => {
+        const userInfo = {
+            fullname : data.fullname,
+            email:data.email,
+            password:data.password
+        }
+      await   axios.post("http://localhost:4001/user/signup",userInfo)
+        .then((res)=>{
+            console.log(res.data)
+            if(res.data){
+                alert("signup Successfull")
+            }
+            localStorage.setItem("Users",JSON.stringify(res.data.user))
+        }).catch((err)=>{
+           if(err.response){
+            alert("Error:"+err.response.data.message)
+           } 
+        })
+    }
 
     return (
         <>
@@ -61,10 +80,10 @@ function Signup() {
                             <input
                                 type="text"
                                 placeholder='Enter your Full Name'
-                                {...register("name", { required: true })}
+                                {...register("fullname", { required: true })}
                                 className='w-80 px-3 border rounded-md outline-none'
                             />
-                            {errors.name && <span className="text-red-500">This field is required</span>}
+                            {errors.fullname && <span className="text-red-500">This field is required</span>}
                         </div>
                         <div className='mt-4 space-y-2'>
                             <span>Email</span>
